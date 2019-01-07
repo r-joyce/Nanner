80ea416a3d86f492e62e3f7ed567742b09393fa6
Ryan <ryan.joyce@wsu.edu>
2019-01-07 11:51:17 -0800

    Moved from JSON Server to Express

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router();
-const middlewares = jsonServer.defaults("readOnly");
+const Joi = require('joi');
+const express = require('express');
+const app = express();
 
-server.use(middlewares);
-server.get('/api/*', (req, res) => {
-	var fs = require('fs');
-	ip = req.url.replace("/api/", "");
+app.use(express.json());
+
+app.post('/api/nmap', (req, res) => {
+	// Define the expected schema
+	const schema = {
+		ip: Joi.string().required(),
+		args: Joi.string().allow('').optional()
+	};
+
+	// Validate request against schema
+	const result = Joi.validate(req.body, schema);
+	if (result.error) {
+		res.status(400).send(result.error);
+		return;
+	}
+
+	const ip = req.body.ip;
+	let args = req.body.args;
+	if (args) {
+		args = args + " ";
+	}
+
+	// Log the request to the server console
+	var time = new Date().toISOString();
+	process.stdout.write(`[${time}] [${req.connection.remoteAddress}] nmap ${args}${ip}... `);
+
+	// Perform the scan
 	var nmap = require('child_process').spawnSync(
-		'./scan.sh', [ip]
+		'./nmap/scan.sh', [ip, args]
 	);
 
-	if (nmap.stderr != null) {
-		console.log(nmap.stderr.toString('utf8'));
-	}
-
+	// Send results of scan
 	if (nmap.stdout != null) {
+		console.log('✔');
 		res.write(nmap.stdout.toString('utf8'));
+	} else if (nmap.stderr != null) {
+		console.log('✖️');
+		res.status(400).send(nmap.stderr.toString('utf8'));
 	} else {
-		console.log('error processing for: ' + ip);
+		console.log('✖️');
+		res.status(400).send(`error processing scan: nmap ${args}${ip}`);
 	}
-
-  res.end();
+	res.end();
 });
 
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
